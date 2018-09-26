64b3444b9f49f8a8770da7421a8331001d606405
Francesco Sardara <francesco.sardara@ausy.be>
2018-09-26 19:37:21 +0200

    Remove unneded wrapping quotes.

--- tests/src/Functional/Lock/RedisLockFunctionalTest.php
+++ tests/src/Functional/Lock/RedisLockFunctionalTest.php
@@ -41,7 +41,7 @@ class RedisLockFunctionalTest extends LockFunctionalTest {
     file_put_contents($filename, $contents);
     $settings = Settings::getAll();
     $settings['container_yamls'][] = drupal_get_path('module', 'redis') . '/example.services.yml';
-    $settings['redis.connection']['interface'] = '\'' .  $redis_interface . '\'';
+    $settings['redis.connection']['interface'] = $redis_interface;
     new Settings($settings);
     OpCodeCache::invalidate(DRUPAL_ROOT . '/' . $filename);
 
